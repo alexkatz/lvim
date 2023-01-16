3fd7bf2f333c1191cc5afc9d8b62537efe3f9d02
Alex Katz <alexanderikatz@gmail.com>
2023-01-15 23:51:15 -0500

    whichkey o for only

--- lua/user/undotree.lua
+++ lua/user/undotree.lua
@@ -1,2 +1,1 @@
 lvim.builtin.which_key.mappings.u = { '<Cmd>UndotreeToggle<CR> <Cmd>UndotreeFocus<CR>', 'Undotree' }
-lvim.builtin.which_key.mappings.o = { '<Cmd>only<CR>', 'Close all other windows' }
--- lua/user/which-key.lua
+++ lua/user/which-key.lua
@@ -4,3 +4,10 @@ lvim.builtin.which_key.mappings.v = { '<C-w>v', 'Vertical Split' }
 -- press leader h in visual mode to engage a search with highlighted text
 -- needs a bit of work to escape certain characters properly
 lvim.builtin.which_key.vmappings.h = { [[y:let @/=fnameescape("<C-R>"") <bar> :set hls <CR>]], 'Search visual text' }
+
+-- leader w or b to eliminate all but current window or buffer
+lvim.builtin.which_key.mappings.o = {
+  name = 'Only',
+  w = { '<Cmd>only<CR>', 'Window' },
+  b = { '<Cmd>BufferLineCloseLeft<CR> <Cmd>BufferLineCloseRight<CR>', 'Buffer' },
+}
